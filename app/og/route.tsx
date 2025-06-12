5041de88bb5c8ac2e7cb63f789f62e583eb6baa0
Ed Urban <edurbancodes@gmail.com>
2025-06-12 16:28:59 -0400

    update package file and og route styling

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,14 +1,21 @@
 import { ImageResponse } from "next/og";
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
   let url = new URL(request.url);
-  let title = url.searchParams.get("title") || "Edward Urban's Portfolio";
+  let title = url.searchParams.get("title") || "Portfolio";
+
+  // Load the avatar image
+  const avatarData = await fetch(new URL("/avatar.jpeg", url.origin)).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">{title}</h2>
+        <div tw="flex flex-col items-center justify-center p-8">
+          <div tw="flex flex-row items-center gap-12 mb-8">
+            <img src={avatarData as any} width={120} height={120} alt="Edward Urban Avatar" style={{ borderRadius: "50%", marginRight: "12px" }} />
+            <h1 tw="text-6xl font-semibold tracking-tighter">Edward Urban</h1>
+          </div>
+          <h2 tw="text-4xl font-semibold tracking-tight text-center">{title}</h2>
         </div>
       </div>
     ),
